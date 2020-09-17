287624ff3c5b4ed92a021e22ed91f5fc060258b1
Sheldon Pasciak <sheldon@pasciak.com>
2020-09-17 09:23:02 -0600

    refactoring
    todo - update all in context viewing - i.e. after add, update or delete, to individual adds and removes from DOM after success.

--- 05-forms/formsService.js
+++ 05-forms/formsService.js
@@ -1,4 +1,10 @@
+/*
 
+    This Javascript file contains the methods for teamsService and BlogsService
+
+    Sheldon Pasciak, September 2020
+
+*/
 
 var teamsService = {
     endpoint: "https://api.remotebootcamp.dev/api/entities/teams"
@@ -8,7 +14,32 @@ var blogsService = {
     endpoint: "https://api.remotebootcamp.dev/api/blogs"
 };
 
+/*
 
+get a specific team record
+
+    var teamId = 5325235;
+
+*/
+teamsService.get = (teamId) => {
+    console.log("teamsService.get is executing ", teamId);
+    const config = {
+        method: "GET",
+        url: teamsService.endpoint + "/" + teamId,
+        crossdomain: true,
+        headers: { "Content-Type": "application/json" }
+    };
+
+    return axios(config);
+};
+
+
+
+/*
+
+get all team records
+
+*/
 teamsService.getAll = () => {
     console.log("teamsService.getAll is executing ");
     const config = {
@@ -21,6 +52,17 @@ teamsService.getAll = () => {
     return axios(config);
 };
 
+/*
+
+add a new team
+
+    var payload = {
+        team: "team name",
+        sport: "sport name",
+        city: "city name"
+    }
+
+*/
 teamsService.add = payload => {
     console.log("teamsService.add is executing ", payload);
 
@@ -32,9 +74,26 @@ teamsService.add = payload => {
         headers: { "Content-Type": "application/json" }
     };
 
+    // NOTE -- a transform might be nice because I'd like to also get back the payload I sent initially in addition to the returned new ID
+
     return axios(config);
 };
 
+
+/*
+
+update an existing team
+
+    var teamId = 1353252;
+
+    var payload = {
+        team: "team name",
+        sport: "sport name",
+        city: "city name"
+    }
+
+
+*/
 teamsService.update = (teamId, payload) => {
     console.log("teamsService.update is executing ", teamId, payload);
 
@@ -49,6 +108,13 @@ teamsService.update = (teamId, payload) => {
     return axios(config);
 };
 
+/*
+
+delete a team entity
+
+    var teamId = 1145591531;
+
+*/
 teamsService.delete = (teamId) => {
     console.log("teamsService.delete is executing ", teamId);
 
@@ -62,6 +128,13 @@ teamsService.delete = (teamId) => {
     return axios(config);
 };
 
+/*
+
+get a specific blog by id
+
+    var blogIdToGet = 1529585106;
+
+*/
 blogsService.get = (blogId) => {
     console.log("blogsService.get is executing ", blogId);
     const config = {
@@ -74,6 +147,21 @@ blogsService.get = (blogId) => {
     return axios(config);
 };
 
+
+/*
+
+updates an existing blog
+
+payload example 
+
+    var updatedBlog = {
+            "id": 1529585106,
+            "authorId": 770304984,
+            "title": "Updated Blog Title " + Date.now(),
+            "content": "Updated Content. " + Date.now()
+        }
+
+*/
 blogsService.update = payload => {
     console.log("blogsService.update is executing ", payload);
 
@@ -89,6 +177,11 @@ blogsService.update = payload => {
 };
 
 
+/*
+
+get all blogs
+
+*/
 blogsService.getAll = () => {
     console.log("blogsService.getAll is executing ");
     const config = {
@@ -101,6 +194,20 @@ blogsService.getAll = () => {
     return axios(config);
 };
 
+
+/*
+
+add's a new blog
+
+payload example:
+
+    var newBlog = {
+        "authorId": 770304984,
+        "title": "Title Of Blog " + Date.now(),
+        "content": "This is my blog content."
+    }    
+
+*/
 blogsService.add = payload => {
     console.log("blogsService.add is executing ", payload);
 
@@ -114,4 +221,3 @@ blogsService.add = payload => {
 
     return axios(config);
 };
-
